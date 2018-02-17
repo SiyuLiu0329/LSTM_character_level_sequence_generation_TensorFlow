10203db8f933426259fa43647a00c1a5dd5d38d4
Siyu Liu <siyu.liu2@uqconnect.edu.au>
2018-02-17 18:33:56 +1000

    minor fixes

--- lstm_model.py
+++ lstm_model.py
@@ -3,7 +3,7 @@ import random
 import string
 import numpy as np
 
-
+RNN_SIZE = 512
 
 class Cell:
     def __init__(self, n_units, n_output, input_tensor):
@@ -67,27 +67,26 @@ def process_text(data):
     return x_train, y_train, char_to_ix, ix_to_char
 
 
-def to_batches(data, batch_size):
+def prepare_training_set(data, num_chars=27):
     batches = []
-    for i in range(1536):
-        batches.append(data[:, i, :].reshape((-1, 1, 27)))
+    for i in range(len(data[0])):
+        batches.append(data[:, i, :].reshape((-1, 1, num_chars)))
     return batches
-    
 
-def generate_sequence(w, b, w_out, b_out, ix_to_char):
-    state = np.zeros((1, 256))
-    current_input = np.zeros((1, 27))
+def generate_sequence(w, b, w_out, b_out, ix_to_char, max_len = 30, num_chars=27):
+    state = np.zeros((1, RNN_SIZE))
+    current_input = np.zeros((1, num_chars))
     seq = []
     idx = -1
-    while len(seq) <= 30:
+    while len(seq) <= max_len:
         if idx == 0:
             break
         current_input = np.concatenate((state, current_input), axis=1)
         state = np.tanh(np.dot(current_input, w) + b)
         logits = np.dot(state, w_out) + b_out
         pred = softmax(logits)
-        idx = np.random.choice([i for i in range(27)], p = pred.ravel())
-        current_input = np.zeros((1, 27))
+        idx = np.random.choice([i for i in range(len(ix_to_char))], p = pred.ravel())
+        current_input = np.zeros((1, len(ix_to_char)))
         current_input[0][idx] = 1
         seq.append(idx)
     
@@ -95,48 +94,50 @@ def generate_sequence(w, b, w_out, b_out, ix_to_char):
     print(''.join(seq))
 
 def softmax(x):
-    """Compute softmax values for each sets of scores in x."""
     e_x = np.exp(x - np.max(x))
     return e_x / e_x.sum()
 
 
-if __name__ ==  '__main__':
-    data = read_file('txt/dinos.txt')
-    x_train, y_train, char_to_ix, ix_to_char = process_text(data)
-    x_batches, y_batches = to_batches(x_train, 1), to_batches(y_train, 1)
-
-    input_tensor = tf.placeholder(tf.float64, shape=[28, 1, 27])
-    # input_tensor = tf.constant([[[1,2,3,4,5], [6,7,8,9,10]], [[1.1,2,3,4,5], [6,7,8,9,10]]], shape=[2, 2, 5], dtype=tf.float64)
-    cell = Cell(256, 27, input_tensor)
-
+def build_model(cell, labels, n_time_steps=28, lr= 0.002):
     losses = []
-    labels = tf.placeholder(tf.float64, [None, 1, 27])
-    for i in range(28):
+    for i in range(n_time_steps):
         pred, logits = cell.forward_pass()
         label = labels[i, :, :]
         losses.append(tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits, labels=label))
 
     global_step = tf.Variable(0, trainable=False)
-    starter_learning_rate = 0.002
+    starter_learning_rate = lr
     learning_rate = tf.train.exponential_decay(starter_learning_rate, global_step,
                                        2000, 0.96, staircase=True)
     total_loss = tf.reduce_sum(losses)
-    optimizer = tf.train.AdamOptimizer(learning_rate)
-    gradients, variables = zip(*optimizer.compute_gradients(total_loss))
-    gradients, _ = tf.clip_by_global_norm(gradients, 5.0)
-    optimize = optimizer.apply_gradients(zip(gradients, variables), global_step=global_step)
+    optimiser = tf.train.AdamOptimizer(learning_rate)
+    gradients, variables = zip(*optimiser.compute_gradients(total_loss))
+    gradients, _ = tf.clip_by_global_norm(gradients, 2.0)
+    optimise = optimiser.apply_gradients(zip(gradients, variables), global_step=global_step)
+    return optimise, total_loss
+
+if __name__ ==  '__main__':
+    data = read_file('txt/dinos.txt')
+    x_train, y_train, char_to_ix, ix_to_char = process_text(data)
+    num_chars = len(ix_to_char)
+    x_train = prepare_training_set(x_train, num_chars=num_chars)
+    y_train = prepare_training_set(y_train, num_chars=num_chars)
+
+    input_tensor = tf.placeholder(tf.float64, shape=[None, 1, num_chars])
+    cell = Cell(RNN_SIZE, 27, input_tensor)
+    labels = tf.placeholder(tf.float64, [None, 1, len(ix_to_char)])
+    optimise_opt, total_loss = build_model(cell, labels)
 
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         i = 0
         while True:
             idx = i % 1536
-            sess.run(optimize, feed_dict={input_tensor: x_batches[idx], labels: y_batches[idx]})
+            sess.run(optimise_opt, feed_dict={input_tensor: x_train[idx], labels: y_train[idx]})
             if i % 100 == 0:
                 print('iter=' + str(i))
-                print(sess.run(total_loss, feed_dict={input_tensor: x_batches[idx], labels: y_batches[idx]}))
+                print(sess.run(total_loss, feed_dict={input_tensor: x_train[idx], labels: y_train[idx]}))
                 w, b, w_out, b_out = cell.get_weights()
                 w, b, w_out, b_out = sess.run([w, b, w_out, b_out])
-                generate_sequence(w, b, w_out, b_out, ix_to_char)
-
+                generate_sequence(w, b, w_out, b_out, ix_to_char, max_len = 26, num_chars=num_chars)
             i += 1
